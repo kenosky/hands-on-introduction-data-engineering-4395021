20e3196520f998c9d23f462f7870558182cebef1
Kehinde Adeoye <kenosky@users.noreply.github.com>
2023-05-31 19:51:59 +0000

    automating manual data extract from api source using airflow

--- airflow/dags/extract_dag.py
+++ airflow/dags/extract_dag.py
@@ -1,1 +1,14 @@
+from airflow.operators.bash import BashOperator
+from airflow import DAG
+from datetime import datetime
 
+with DAG(
+    "extract_dag",
+    schedule_interval=None,
+    start_date=datetime(2023, 5, 3),
+    catchup=False,
+) as dag:
+    extract_task = BashOperator(
+        task_id="extract_task",
+        bash_command="wget -c https://datahub.io/core/top-level-domain-names/r/top-level-domain-names.csv.csv -O /workspaces/hands-on-introduction-data-engineering-4395021//lab/manual/manual-extract-data.csv",
+    )
